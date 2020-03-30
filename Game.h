ce20546ceb85567537aab3a96d6b4409f1cabb8b
Craig Gault <craig.g.gault@gmail.com>
2020-03-30 15:06:25 +0100

    Added spacing for function types

--- Game.h
+++ Game.h
@@ -6,17 +6,23 @@ class Game
 {
 public:
 	void StartGame();
+
 	char state[3][3];
-	void turn();
+	void scan_state(char state[3][3]);
+	void amend_state();
+
+
 	int row_played = 0;
 	int column_played = 0;
-	void amend_state();
+
+	void turn();
 	int WhoseTurn;
+
 	void SwitchTurn();
 	void InputPosition();
+
 	bool GameOver = false;
 	int move_count[2] = {1};
-	void scan_state(char state[3][3]);
 	bool draw = false;
 
 private:
